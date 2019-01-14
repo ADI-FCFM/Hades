9e5921ec2ad6798b31b7ffe1fc4d013466b9d467
kyra <kyrantonia@gmail.com>
2019-01-14 17:37:24 -0300

    crear clase Puerta

---
+++ lib/puerta.dart
@@ -0,0 +1,29 @@
+class Puerta{
+  String id;
+  String ubicacion;
+  String descripcion;
+  String modo;
+  String modo_texto;
+  bool puede_abrir;
+
+  Puerta({
+    this.id,
+    this.ubicacion,
+    this.descripcion,
+    this.modo,
+    this.modo_texto,
+    this.puede_abrir
+  });
+
+  factory Puerta.fromJson(Map<String, dynamic> parsedJson){
+    return Puerta(
+        id: parsedJson['id'],
+        ubicacion : parsedJson['ubicacion'],
+        descripcion : parsedJson ['descripcion'],
+        modo: parsedJson['modo'],
+        modo_texto: parsedJson['modo_texto'],
+        puede_abrir: parsedJson['puerta_abrir']
+
+    );
+  }
+}
