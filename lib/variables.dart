3ededf373248ba28f7b8dbdcc17dd2635b4084ac
kyra <kyrantonia@gmail.com>
2019-01-21 16:13:32 -0300

    cambiar urls

--- lib/variables.dart
+++ lib/variables.dart
@@ -1,11 +1,8 @@
-/*String urlRefrescar = 'http://172.17.85.185:8000/refrescar_token';
+String urlRefrescar = 'http://172.17.85.185:8000/refrescar_token';
 String urlInicio = 'http://172.17.85.185:8000/';
 String urlPuertas ='http://172.17.85.185:8000/puertas?token=';
-*/
+String urlCerrarSesion = 'http://172.17.85.185:8000/salir';
+
 String urlUri =
     'https://sys21.adi.ing.uchile.cl/~jarriagada/davinci/web/batch/heimdall/login?uri=unilinks://accesos.app.adi/path/subpath';
 
-String urlRefrescar = 'http://172.17.85.189:8000/refrescar_token';
-String urlInicio = 'http://172.17.85.189:8000/';
-String urlPuertas = 'http://172.17.85.189:8000/puertas?token=';
-String urlCerrarSesion = 'http://172.17.85.189:8000/salir';
