4ce86a72fa52691cfdd497739c52a4090867a6d3
kyra <kyrantonia@gmail.com>
2019-01-29 10:43:18 -0300

    borrar variables

--- lib/variables.dart
+++
@@ -1,9 +0,0 @@
-String urlRefrescar = 'http://172.17.85.185:8000/refrescar_token';
-String urlInicio = 'http://172.17.85.185:8000/';
-String urlAccesos = 'http://172.17.85.185:8000/puertas';
-
-String urlCerrarSesion = 'http://172.17.85.185:8000/salir';
-
-String urlUri =
-    'https://sys21.adi.ing.uchile.cl/~jarriagada/davinci/web/batch/heimdall/login?uri=unilinks://accesos.app.adi/path/subpath';
-String tituloAplicacion = 'Administrador de Accesos';
